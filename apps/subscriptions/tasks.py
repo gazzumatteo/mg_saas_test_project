0d3c2c9e6a36094d52927aac55fdfa8c7aeeed26
gazzumatteo <dev@duckma.com>
2024-11-25 12:46:09 +0000

    Pegasus initial. Version 2024.11.3

---
+++ apps/subscriptions/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from stripe.error import StripeError
+
+from .helpers import sync_subscription_model_with_stripe
+from apps.users.models import CustomUser
+
+
+@shared_task
+def sync_subscriptions_task():
+    for user in CustomUser.get_items_needing_sync():
+        try:
+            sync_subscription_model_with_stripe(user)
+        except StripeError as e:
+            from sentry_sdk import capture_exception
+
+            capture_exception(e)
